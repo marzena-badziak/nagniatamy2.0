447bbaf85db24b0ae074d8c9efada4b9448cb676
marzena-badziak <marzena.badziak@gmail.com>
2017-02-22 01:36:37 +0100

    Upgraded blur effect

--- app.js
+++ app.js
@@ -53,51 +53,28 @@ jQuery(window).on('resize', function(){
 };
 stickyMenu();
 
+
+function changeBlur(scrollPercentage) {
+	var PictureToBlur = jQuery('.imgContainer');
+	if(scrollPercentage < 8) {
+		PictureToBlur.css("filter", "blur(0px)");
+	}
+	PictureToBlur.css("filter", "blur(" + Math.floor(scrollPercentage / 10) + "px)");
+	console.log(scrollPercentage);
+}
+
 function blurredPicture() {
   jQuery(document).scroll(function() {
     var windowHeight = jQuery(window).height();
     // console.log('wysokosc okna:' + windowHeight);
     var scrollValue = jQuery(document).scrollTop();
     var scrollPercentage = (scrollValue / windowHeight) * 100;
-    // console.log("procent: " + scrollPercentage);
-    var PictureToBlur = jQuery('.imgContainer');
+    //  console.log("procent: " + scrollPercentage)
 
-    if (scrollPercentage > 5) {
-      PictureToBlur.addClass('blur1');
-    }
-    else {
-      PictureToBlur.removeClass('blur1');
-    };
-    if (scrollPercentage > 10) {
-      PictureToBlur.addClass('blur2');
-    }
-    else {
-      PictureToBlur.removeClass('blur2');
-    };
-    if (scrollPercentage > 20) {
-      PictureToBlur.addClass('blur3');
-    }
-    else {
-      PictureToBlur.removeClass('blur3');
-    };
-    if (scrollPercentage > 30) {
-      PictureToBlur.addClass('blur4');
-    }
-    else {
-      PictureToBlur.removeClass('blur4');
-    };
-    if (scrollPercentage > 40) {
-      PictureToBlur.addClass('blur5');
-    }
-    else {
-      PictureToBlur.removeClass('blur5');
-    };
-
-
+    changeBlur(scrollPercentage);
 
   });
 };
-
 blurredPicture();
 
 });
